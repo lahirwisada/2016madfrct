21246c23e52277172872704406520938bf670e29
aldiyah <rinaldi79@gmail.com>
2017-01-17 18:28:30 +0700

    Penambahan fungsi

--- application/models/model_master_pangkat.php
+++ application/models/model_master_pangkat.php
@@ -14,15 +14,31 @@ class Model_master_pangkat extends Master_pangkat {
     public function __construct() {
         parent::__construct();
     }
+
     public function all($force_limit = FALSE, $force_offset = FALSE) {
         return parent::get_all(array(
                     "ur_pangkat",
                         ), FALSE, TRUE, FALSE, 1, TRUE, $force_limit, $force_offset);
     }
-    
-    public function get_detail_by_kode_pangkat($kode_pangkat = ''){
-        if($kode_pangkat != ''){
-            return $this->get_detail("kode_pangkat LIKE '%".$kode_pangkat."%'");
+
+    public function check_id_by_kode_pangkat_and_insert($kode_pangkat = NULL) {
+        $id_pangkat = FALSE;
+        if ($kode_pangkat) {
+            $pangkat_found = $this->get_detail("lower(kode_pangkat) LIKE lower('%" . $kode_pangkat . "%')");
+            if ($pangkat_found) {
+                $id_pangkat = $pangkat_found->id_pangkat;
+            } else {
+                $this->kode_pangkat = $kode_pangkat;
+                $id_pangkat = $this->save();
+            }
+            unset($pangkat_found);
+        }
+        return $id_pangkat;
+    }
+
+    public function get_detail_by_kode_pangkat($kode_pangkat = '') {
+        if ($kode_pangkat != '') {
+            return $this->get_detail("lower(kode_pangkat) LIKE lower('%" . $kode_pangkat . "%')");
         }
         return FALSE;
     }

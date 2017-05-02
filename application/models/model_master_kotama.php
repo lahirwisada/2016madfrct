a652f4a0d4c91985b2cc50bf418ef2ea1f1916df
aldiyah <rinaldi79@gmail.com>
2017-05-02 14:03:40 +0700

    Update Master Kotama

--- application/models/model_master_kotama.php
+++ application/models/model_master_kotama.php
@@ -9,6 +9,7 @@ class Model_master_kotama extends Master_kotama {
     protected $rules = array(
         array("kode_kotama", "required|min_length[1]"),
         array("ur_kotama", "required|min_length[3]|max_length[300]"),
+        array("struktur_kotama", "required|numeric"),
     );
 
     public function __construct() {
@@ -18,6 +19,7 @@ class Model_master_kotama extends Master_kotama {
     public function all($force_limit = FALSE, $force_offset = FALSE) {
         return parent::get_all(array(
                     "ur_kotama",
+                    "struktur_kotama",
                         ), FALSE, TRUE, FALSE, 1, TRUE, $force_limit, $force_offset);
     }
 

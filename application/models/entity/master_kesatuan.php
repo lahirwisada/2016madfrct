b10da19baf3bfa8bdd646e3d4d8c0cb364a5f1dc
aldiyah <rinaldi79@gmail.com>
2017-05-02 14:05:54 +0700

    Update Master Kesatuan

--- application/models/entity/master_kesatuan.php
+++ application/models/entity/master_kesatuan.php
@@ -4,20 +4,20 @@ if (!defined("BASEPATH")) {
     exit("No direct script access allowed");
 }
 
-class Master_satuan extends LWS_model {
+class Master_kesatuan extends LWS_model {
 
     public function __construct() {
-        parent::__construct("master_satuan");
-        $this->primary_key = "id_satuan";
+        parent::__construct("master_kesatuan");
+        $this->primary_key = "id_kesatuan";
         
         $this->attribute_labels = array_merge_recursive($this->_continuously_attribute_label, $this->attribute_labels);
         $this->rules = array_merge_recursive($this->_continuously_rules, $this->rules);
     }
 
     protected $attribute_labels = array(
-        "id_satuan" => array("id_satuan", "Id Satuan"),
-        "kode_satuan" => array("kode_satuan", "Kode Satuan"), 
-        "nama_satuan" => array("nama_satuan", "Nama Satuan"),
+        "id_kesatuan" => array("id_kesatuan", "ID Kesatuan"),
+        "kode_kesatuan" => array("kode_kesatuan", "Kode Kesatuan"), 
+        "nama_kesatuan" => array("nama_kesatuan", "Nama Kesatuan"),
 //        "created_date" => array("created_date", "created_date"),
 //        "created_by" => array("created_by", "created_by"),
 //        "modified_date" => array("modified_date", "modified_date"),
@@ -25,9 +25,9 @@ class Master_satuan extends LWS_model {
 //        "record_active" => array("record_active", "record_active"),
     );
     protected $rules = array(
-        array("id_satuan", ""),
-        array("kode_satuan", ""),
-        array("nama_satuan", ""),
+        array("id_kesatuan", ""),
+        array("kode_kesatuan", ""),
+        array("nama_kesatuan", ""),
 //        array("created_date", ""),
 //        array("created_by", ""),
 //        array("modified_date", ""),

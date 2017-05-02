0f3bdd9792b506e6563be03570da64ec98b1c684
aldiyah <rinaldi79@gmail.com>
2017-05-02 14:04:07 +0700

    Update Master Kotama

--- application/views/back_end/mskotama/detail.php
+++ application/views/back_end/mskotama/detail.php
@@ -20,21 +20,32 @@ $detail = isset($detail) ? $detail : FALSE;
                     <div class="form-group">
                         <label class="col-md-3 col-xs-12 control-label">Kode Kotama *</label>
                         <div class="col-md-6 col-xs-12">
-                                <input type="text" name="kode_kotama" class="form-control" value="<?php echo $detail ? $detail->kode_kotama : ""; ?>">                                   
+                            <input type="text" name="kode_kotama" class="form-control" value="<?php echo $detail ? $detail->kode_kotama : ""; ?>">                                   
                             <span class="help-block">Isikan sesuai dengan kode kotama.</span>
                         </div>
                     </div>
                     <div class="form-group">
-                        <label class="col-md-3 col-xs-12 control-label">UR Kotama *</label>
+                        <label class="col-md-3 col-xs-12 control-label">Uraian Kotama *</label>
                         <div class="col-md-6 col-xs-12">
-                                <input type="text" name="ur_kotama" class="form-control" value="<?php echo $detail ? $detail->ur_kotama : ""; ?>">                                        
+                            <input type="text" name="ur_kotama" class="form-control" value="<?php echo $detail ? $detail->ur_kotama : ""; ?>">                                        
                             <span class="help-block">Isikan sesuai dengan UR kotama.</span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 col-xs-12 control-label">Struktur *</label>
+                        <div class="col-md-6 col-xs-12">        
+                            <select name="struktur_kotama" class="form-control">
+                                <option value="">Pilih Struktur</option>
+                                <option value="1"<?php echo $detail && $detail->struktur_kotama == 1 ? " selected" : ""; ?>>Dalam Struktur</option>
+                                <option value="2"<?php echo $detail && $detail->struktur_kotama == 2 ? " selected" : ""; ?>>Luar Struktur</option>
+                            </select>
+                            <span class="help-block">Isikan sesuai dengan Struktur.</span>
                         </div>
                     </div>
                 </div>
                 <div class="panel-footer">
                     <button type="submit" class="btn-primary btn pull-right">Submit</button>
-                    <a href="<?php echo base_url("back_end/".$active_modul."/index"); ?>" class="btn-default btn">Batal / Kembali</a>
+                    <a href="<?php echo base_url("back_end/" . $active_modul . "/index"); ?>" class="btn-default btn">Batal / Kembali</a>
                 </div>
             </div>
         </form>

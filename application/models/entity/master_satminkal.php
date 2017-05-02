f4caf0a6a76bb1fecbdce2d49742a1e553207fc1
aldiyah <rinaldi79@gmail.com>
2017-05-02 14:07:49 +0700

    Update Master Satminkal

--- application/models/entity/master_satminkal.php
+++ application/models/entity/master_satminkal.php
@@ -1,17 +1,23 @@
 <?php
+
 if (!defined("BASEPATH")) {
     exit("No direct script access allowed");
 }
+
 class Master_satminkal extends LWS_model {
+
     public function __construct() {
         parent::__construct("master_satminkal");
         $this->primary_key = "id_satminkal";
     }
+
     protected $attribute_labels = array(
-        "id_satminkal" => array("id_satminkal", "Id Satminkal"),
+        "id_satminkal" => array("id_satminkal", "ID Satminkal"),
         "id_kotama" => array("id_kotama", "Id Kotama"),
         "kode_satminkal" => array("kode_satminkal", "Kode"),
         "ur_satminkal" => array("ur_satminkal", "satminkal"),
+        "id_kesatuan" => array("id_kesatuan", "ID Kesatuan"),
+        "id_corps" => array("id_corps", "ID Corps"),
         "created_date" => array("created_date", "created_date"),
         "created_by" => array("created_by", "created_by"),
         "modified_date" => array("modified_date", "modified_date"),
@@ -23,6 +29,8 @@ class Master_satminkal extends LWS_model {
         array("id_kotama", ""),
         array("kode_satminkal", ""),
         array("ur_satminkal", ""),
+        array("id_kesatuan", ""),
+        array("id_corps", ""),
         array("created_date", ""),
         array("created_by", ""),
         array("modified_date", ""),
@@ -38,7 +46,26 @@ class Master_satminkal extends LWS_model {
             ),
             "referenced" => "LEFT"
         ),
+        "master_kesatuan" => array(
+            "fkey" => "id_kesatuan",
+            "columns" => array(
+                "kode_kesatuan",
+                "nama_kesatuan",
+            ),
+            "referenced" => "LEFT"
+        ),
+        "master_corps" => array(
+            "fkey" => "id_corps",
+            "columns" => array(
+                "kode_corps",
+                "init_corps",
+                "ur_corps",
+            ),
+            "referenced" => "LEFT"
+        ),
     );
     protected $attribute_types = array();
+
 }
+
 ?>

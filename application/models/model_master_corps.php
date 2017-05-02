6b0ebb1f6992065c1570cca740517ed5555328b6
aldiyah <rinaldi79@gmail.com>
2017-05-02 13:49:17 +0700

    Update Master Corps - Index

--- application/models/model_master_corps.php
+++ application/models/model_master_corps.php
@@ -15,10 +15,22 @@ class Model_master_corps extends Master_corps {
     public function __construct() {
         parent::__construct();
     }
+
     public function all($force_limit = FALSE, $force_offset = FALSE) {
         return parent::get_all(array(
                     "ur_corps",
                         ), FALSE, TRUE, FALSE, 1, TRUE, $force_limit, $force_offset);
+    }
+
+    public function get_like($keyword = FALSE) {
+
+        $result = FALSE;
+        if ($keyword) {
+            $this->db->order_by("kode_corps", "asc");
+            $this->db->where(" lower(" . $this->table_name . ".ur_corps) LIKE lower('%" . $keyword . "%') OR lower(" . $this->table_name . ".kode_corps) LIKE lower('%" . $keyword . "%')", NULL, FALSE);
+            $result = $this->get_where();
+        }
+        return $result;
     }
 
 }

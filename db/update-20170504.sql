6ca89e7cfc317f94010a8ce9adf0c57655860aa7
aldiyah <rinaldi79@gmail.com>
2017-05-04 21:48:34 +0700

    Update Database 20170504
    Update database per tanggal 4 Mei 2017

---
+++ db/update-20170504.sql
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="Content-Script-Type" content="text/javascript">
+<meta name="robots" content="noindex">
+<meta name="referrer" content="origin-when-crossorigin">
+<title>Login - Adminer</title>
+<link rel="stylesheet" type="text/css" href="adminer.php?file=default.css&amp;version=4.3.1">
+<script type="text/javascript" src="adminer.php?file=functions.js&amp;version=4.3.1"></script>
+<link rel="shortcut icon" type="image/x-icon" href="adminer.php?file=favicon.ico&amp;version=4.3.1">
+<link rel="apple-touch-icon" href="adminer.php?file=favicon.ico&amp;version=4.3.1">
+
+<body class="ltr nojs" onkeydown="bodyKeydown(event);" onclick="bodyClick(event);">
+<script type="text/javascript">
+document.body.className = document.body.className.replace(/ nojs/, ' js');
+var offlineMessage = 'You are offline.';
+</script>
+
+<div id="help" class="jush-pgsql jsonly hidden" onmouseover="helpOpen = 1;" onmouseout="helpMouseout(this, event);"></div>
+
+<div id="content">
+<h2>Login</h2>
+<div id='ajaxstatus' class='jsonly hidden'></div>
+<div class='error'>Unable to connect to PostgreSQL server: fe_sendauth: no password supplied<br>Master password expired. <a href="https://www.adminer.org/en/extension/" target="_blank">Implement</a> <code>permanentLogin()</code> method to make it permanent.</div>
+<form action='' method='post'>
+<table cellspacing="0">
+<tr><th>System<td><select name='auth[driver]'><option value="server">MySQL<option value="sqlite">SQLite 3<option value="sqlite2">SQLite 2<option value="pgsql" selected>PostgreSQL<option value="oracle">Oracle<option value="mssql">MS SQL<option value="firebird">Firebird (alpha)<option value="simpledb">SimpleDB<option value="mongo">MongoDB (beta)<option value="elastic">Elasticsearch (beta)</select><tr><th>Server<td><input name="auth[server]" value="" title="hostname[:port]" placeholder="localhost" autocapitalize="off">
+<tr><th>Username<td><input name="auth[username]" id="username" value="postgres" autocapitalize="off">
+<tr><th>Password<td><input type="password" name="auth[password]">
+<tr><th>Database<td><input name="auth[db]" value="db_fcstprsn" autocapitalize="off">
+</table>
+<script type="text/javascript">
+focus(document.getElementById('username'));
+</script>
+<p><input type='submit' value='Login'>
+<label><input type='checkbox' name='auth[permanent]' value='1'>Permanent login</label>
+<div></div>
+</form>
+</div>
+
+<form action='' method='post'>
+<div id='lang'>Language: <select name='lang' onchange="this.form.submit();"><option value="en" selected>English<option value="ar">العربية<option value="bg">Български<option value="bn">বাংলা<option value="bs">Bosanski<option value="ca">Català<option value="cs">Čeština<option value="da">Dansk<option value="de">Deutsch<option value="el">Ελληνικά<option value="es">Español<option value="et">Eesti<option value="fa">فارسی<option value="fi">Suomi<option value="fr">Français<option value="gl">Galego<option value="hu">Magyar<option value="id">Bahasa Indonesia<option value="it">Italiano<option value="ja">日本語<option value="ko">한국어<option value="lt">Lietuvių<option value="nl">Nederlands<option value="no">Norsk<option value="pl">Polski<option value="pt">Português<option value="pt-br">Português (Brazil)<option value="ro">Limba Română<option value="ru">Русский<option value="sk">Slovenčina<option value="sl">Slovenski<option value="sr">Српски<option value="ta">த‌மிழ்<option value="th">ภาษาไทย<option value="tr">Türkçe<option value="uk">Українська<option value="vi">Tiếng Việt<option value="zh">简体中文<option value="zh-tw">繁體中文</select> <input type='submit' value='Use' class='hidden'>
+<input type='hidden' name='token' value='1016593:273835'>
+</div>
+</form>
+<div id="menu">
+<h1>
+<a href='https://www.adminer.org/' target='_blank' id='h1'>Adminer</a> <span class="version">4.3.1</span>
+<a href="https://www.adminer.org/#download" target="_blank" id="version"></a>
+</h1>
+</div>
+<script type="text/javascript">setupSubmitHighlight(document);</script>
